9312e43bbd1c8386219c89ee2814cdc62f18b713
Michele Barreto <michelebarreto1@hotmail.com>
2021-07-04 23:58:54 -0300

    Projeto Console

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleAppWeb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lista de Números Multiplos de (3) e (5)");
+            
+            for (int num = 0; num <= 100; num++)
+            {
+                
+                //Verificando se o número é multiplo de 3
+                if (num %3 ==0)
+                {
+                    Console.WriteLine(num + " Nome ");
+                    //Verificando se o número é multiplo de 5
+                    if (num %5 ==0)
+                    {
+                        Console.WriteLine(num + " Sobrenome ");
+                        
+                        
+
+                    }
+                }
+            }
+            
+            Console.Write("Pressione alguma tecla para sair !");
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
